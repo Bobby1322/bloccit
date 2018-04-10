500b85380b558663d754191ede2f0cc963f7d27b
Bob-e <bob.e.mercer@gmail.com>
2018-04-09 20:10:42 -0400

    Assignment 30: Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
     has_many :favorites, dependent: :destroy
+    after_create :create_favorite
 
     default_scope { order('rank DESC') }
 
@@ -30,4 +31,9 @@ class Post < ApplicationRecord
         new_rank = points + age_in_days
         update_attribute(:rank, new_rank)
     end
+
+    def after_create
+        Favorite.create(post: self, user: self.user)
+        FavoriteMailler.new_post(self).deliever_now
+    end 
 end
