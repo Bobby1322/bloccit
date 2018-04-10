5070283fc9c66aa76beb503803d7a57da40f713f
Bob-e <bob.e.mercer@gmail.com>
2018-04-09 20:52:58 -0400

    I cant fucking spell to save my life

--- app/models/post.rb
+++ app/models/post.rb
@@ -34,6 +34,6 @@ class Post < ApplicationRecord
 
     def create_favorite
         Favorite.create(post: self, user: self.user)
-        FavoriteMailer.new_post(self).deliever_now
+        FavoriteMailer.new_post(self).deliver_now
     end 
 end
